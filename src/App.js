5a941133416969c0985af6d2bde55eb3da1c59c7
Angg <angelagilhotra@gmail.com>
2018-08-11 18:32:31 +0530

    second

--- src/App.js
+++ src/App.js
@@ -5,67 +5,120 @@ import {
   Text,
   AppBar,
   observe,
-  ContextMenu, 
-  ContextMenuItem,
   AppView,
-  Table, TableHeader, TableRow, TableCell
+  ContextMenu, ContextMenuItem,
+  Table, TableHeader, TableRow, TableCell,
+  Card,
+  SidePanel
 } from '@aragon/ui'
+import { Grid, Row, Col } from 'react-flexbox-grid';
+// import Aragon, { providers } from '@aragon/client'
 import styled from 'styled-components'
-
-class LoginButton extends React.Component {
-  render() {
-    return (
-      <Button> hello </Button>
-    )
-  }
-}
+import AppLayout from './components/AppLayout'
 
 class InvoiceSummary extends React.Component {
-    render () {
-      return (
-        <Table
-    header={
-      <TableRow>
-        <TableHeader title="Activity" />
-      </TableRow>
-    }
-  >
-    <TableRow>
-      <TableCell>
-        <Text>January</Text>
-      </TableCell>
-      <TableCell>
-        <Text>February</Text>
-      </TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell>
-        <Text>10 commits</Text>
-      </TableCell>
-      <TableCell>
-        <Text>32 commits</Text>
-      </TableCell>
-    </TableRow>
-  </Table>
-      )
-    }
-}
-
-export default class App extends React.Component {
   render () {
     return (
-      <AragonApp>
-        <AppView title="Invoice Manager">
-        <InvoiceSummary />
-        </AppView>
-      </AragonApp>
+      <Table
+      header={
+        <TableRow>
+          <TableHeader title="Invoices" />
+        </TableRow>
+      }
+    >
+      <TableRow>
+        <TableCell>
+          <Text>January</Text>
+        </TableCell>
+        <TableCell>
+          <Text>February</Text>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>
+          <Text>10 commits</Text>
+        </TableCell>
+        <TableCell>
+          <Text><ContextMenu>
+            <ContextMenuItem>Some Action</ContextMenuItem>
+            <ContextMenuItem>Another Action</ContextMenuItem>
+        </ContextMenu></Text>
+        </TableCell>
+      </TableRow>
+      </Table>
     )
   }
 }
 
-const ObservedCount = observe(
-  (state$) => state$,
-  { count: 0 }
-)(
-  ({ count }) => <Text.Block style={{ textAlign: 'center' }} size='xxlarge'>{count}</Text.Block>
-)
+class InfoBar extends React.Component {
+  render () {
+    return (
+      <Table
+      header={
+        <TableRow>
+          <TableHeader title="Info" />
+        </TableRow>
+      }
+    >
+      <TableRow>
+        <TableCell>
+          <Text>January</Text>
+        </TableCell>
+        <TableCell>
+          <Text>34</Text>
+        </TableCell>
+      </TableRow>
+      </Table>
+    )
+  }
+}
+
+class CreatePayment extends React.Component {
+  render () {
+    return (
+      <Button mode="strong">Create Payment </Button>
+    )
+  }
+}
+
+class TaxInfo extends React.Component {
+  render() {
+    return (
+      <Card>
+    <Text>Some Text</Text>
+  </Card>
+    )
+  }
+}
+
+
+export default class App extends React.Component {
+  state = {
+    sidepanelOpened: false
+  }
+  render () {
+    return (
+      <AppView
+    appBar={
+      <AppBar
+        title="Invoice Manager"
+        endContent={<CreatePayment />}
+      />
+    }
+  >
+    <Grid fluid>
+        <Row>
+          <Col xs={6} md={10}>
+            <InvoiceSummary />
+          </Col>
+          <Col xs={6} md={2}>
+            <InfoBar />
+          </Col>
+        </Row>
+      </Grid>
+
+
+  </AppView>
+    );
+  }
+}
